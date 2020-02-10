0fe90c755543375431d9cf5941a23544b3434a5c
GENEMATOR SAKHIB <sakhib.orzklv@icloud.com>
2020-02-10 16:44:54 +0500

    Optimizing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import config
 import telebot
-import logging
-import customization
+import logger
+import messages.message
+
+logger.bot_logs()
 
 bot = telebot.TeleBot(config.TOKEN)
 logger = telebot.logger
@@ -11,7 +13,7 @@ print(customization.color.BOLD + customization.color.RED + "Bot started working"
 @bot.message_handler(content_types=['text'])
 def text_message(message):
     if message.text == "/start":
-        bot.reply_to(message, '<b>So this is my brand new message</b>', parse_mode='HTML')
+        bot.reply_to(message, messages.message.start, parse_mode='HTML')
     else:
         bot.reply_to(message, '<b>Please, can you type<b> /help. I can\'t get you')
 
