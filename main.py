9786ecbfcf27c2c88655ecd744c02181221da6db
GENEMATOR SAKHIB <sakhib.orzklv@icloud.com>
2020-02-10 17:10:51 +0500

    Added help message reaction

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import telebot
 import logger
 import messages.message
 
-
-
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def text_message(message):
     if message.text == "/start":
-        bot.reply_to(message, messages.message.start, parse_mode='HTML')
+        bot.reply_to(message, messages.message.start, parse_mode='HTML', disable_web_page_preview=True)
+    elif message.text == "/help":
+        bot.reply_to(message, messages.message.help, parse_mode='HTML', disable_web_page_preview=True)
     else:
         bot.reply_to(message, '<b>Please, can you type<b> /help. I can\'t get you')
 
