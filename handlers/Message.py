9a38be316e3ba15edede79f46ba609d42d7b5e1c
GENEMATOR SAKHIB <sakhib.orzklv@icloud.com>
2020-02-11 02:15:32 +0500

    Porting handlers to package form main

--- handlers/Message.py
+++ handlers/Message.py
@@ -1,18 +1,17 @@
-from core import Bot
+from core.Bot import Bot
 
 class HandleMessage:
     def message(self):
-        bot = Bot()
-        bot.bot()
-
-        @bot.message_handler(content_types=['text'])
+        @Bot.bot.message_handler(content_types=['text'])
         def text_message(message):
             if message.text == "/start":
-                bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
+                Bot.bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
             elif message.text == "/help":
-                bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
+                Bot.bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
             else:
-                bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
+                Bot.bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
             pass
+
+        pass
         pass
     pass
--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 from core.Bot import Bot
 from handlers import HandleMessage
 
-
-@Bot.bot.message_handler(content_types=['text'])
-def text_message(message):
-    if message.text == "/start":
-        Bot.bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
-    elif message.text == "/help":
-        Bot.bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
-    else:
-        Bot.bot.reply_to(message, "Message", parse_mode='HTML', disable_web_page_preview=True)
-    pass
-pass
-
 # Message handler
-# message_handler = HandleMessage()
-# message_handler.message()
+message_handler = HandleMessage()
+message_handler.message()
 
 # Bot
 bot = Bot()
