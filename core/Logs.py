42fea86dfe464a0d8666a470390a278e75b9afa8
GENEMATOR SAKHIB <sakhib.orzklv@icloud.com>
2020-02-11 01:20:45 +0500

    Fixing package variables

--- core/Logs.py
+++ core/Logs.py
@@ -1,17 +1,17 @@
 import logging
 import telebot
-from core import Custom
+from core.Custom import Colors
 
 
 # Outputs debug messages to console.
 class Logs:
     def log(self):
         print(
-            Custom.Colors.BOLD + Custom.Colors.RED + "Check phase of bot completed! Bot is working..." + Custom.Colors.END)
+            Colors.BOLD + Colors.RED + "Check phase of bot completed! Bot is working..." + Colors.END)
         print(
-            Custom.Colors.BOLD + Custom.Colors.YELLOW + "For any further problems or bugs," + Custom.Colors.END)
+            Colors.BOLD + Colors.YELLOW + "For any further problems or bugs," + Colors.END)
         print(
-            Custom.Colors.BOLD + Custom.Colors.YELLOW + "Please, contact with the creator:" + Custom.Colors.END)
-        print(Custom.Colors.BOLD + Custom.Colors.RED + "https://t.me/sakhib_orzklv" + Custom.Colors.END)
+            Colors.BOLD + Colors.YELLOW + "Please, contact with the creator:" + Colors.END)
+        print(Colors.BOLD + Colors.RED + "https://t.me/sakhib_orzklv" + Colors.END)
         telebot.logger.setLevel(logging.DEBUG)
     pass
