6e8f470736b141e441a5d37c850e3e64697face1
GENEMATOR SAKHIB <sakhib.orzklv@icloud.com>
2020-02-10 16:44:21 +0500

    Logging telebot logs

---
+++ logger.py
@@ -0,0 +1,9 @@
+import logging
+import telebot
+import customization
+
+# Outputs debug messages to console.
+def bot_logs():
+    logger = telebot.logger
+    print(customization.color.BOLD + customization.color.RED + "Bot started working" + customization.color.END)
+    telebot.logger.setLevel(logging.DEBUG)
