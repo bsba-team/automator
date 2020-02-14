c3c138a8eb1b7aa54475722a4c31fd660e940981
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2020-02-14 10:31:33 +0500

    Done...

---
+++ templates/templates.py
@@ -0,0 +1,25 @@
+class Template:
+    def start(self):
+        start = open('templates/template/start.html', 'rb').read()
+        return start
+        pass
+    pass
+
+    def help(self):
+        help = open('templates/template/help.html', 'rb').read()
+        return help
+        pass
+    pass
+
+    def about(self):
+        about = open('templates/template/about.html', 'rb').read()
+        return about
+        pass
+    pass
+
+    def error(self):
+        error = open('templates/template/error.html', 'rb').read()
+        return error
+        pass
+    pass
+
