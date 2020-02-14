cdcb3e7fced4e00582ec5ab410d8881f3cb9c1a3
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2020-02-14 13:44:13 +0500

    Dammit, showing config for hosting purposes

---
+++ config.py
@@ -0,0 +1,21 @@
+# ======================================
+#              WARNING!!!               
+# Don't share your any info with anyone!
+# ======================================
+
+# ======================================================
+TOKEN = '978274934:AAEXqu0DcDxulqQg95M6JOtq6h1HQICzNTk'
+# ======================================================
+
+# =====================
+# Bot's Information
+# =====================
+BOT_USERNAME = 'bsba_bot'
+BOT_CHATID = 978274934
+
+# =====================
+# Admin's Information
+# =====================
+ADMIN_USERNAME = 'geno_ferollo'
+ADMIN_CHATID = 854562126
+ADMIN_NICKNAME = 'Genemator Sakhib'
