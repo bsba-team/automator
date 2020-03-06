73efefd4d9f224bc1503c6f006492d0ae3b5bdb4
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2020-03-07 01:38:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-import telegram
-import logging
+# -*- coding: utf-8 -*-
 
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, MessageHandler, Filters
+try:
+    print('Importing "time"...')
+    import time
+    print('Successfully imported "time"')
+    print('Importing "pyTelegramBotApi"...')
+    import telebot
+    print('Successfully imported "pyTelegramBotApi"')
+except ImportError:
+    print('Failed to import required modules, quitting...')
+    exit(1)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-print('Telegram bot is running')
+BOT_TOKEN = ''
 
-BOT_TOKEN = '978274934:AAHR-ZIEXsphxpKEbDUVpk-XAeK78I6ATEc'
-BOT_NAME = 'BSBA™: Main Bot'
-BOT_USERNAME = 'bsba_bot'
-
-updater = Updater(token=BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
-
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
-
-
-def stop(updater):
-    updater.stop()
-
-
-updater.start_polling()
+bot = telebot.TeleBot(token=BOT_TOKEN)
