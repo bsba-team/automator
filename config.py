aff4020effaa51a353ba58d95e9d5ebc67d50029
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2020-03-09 16:42:17 +0500

    Automation

---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Telegram Bot API Token for Python config usage
+token = '978274934:AAHR-ZIEXsphxpKEbDUVpk-XAeK78I6ATEc'
--- main.py
+++ main.py
@@ -3,14 +3,31 @@
 try:
     print('Importing "time"...')
     import time
+
     print('Successfully imported "time"')
+
     print('Importing "pyTelegramBotApi"...')
     import telebot
+
     print('Successfully imported "pyTelegramBotApi"')
+
+    print('Importing "configs"...')
+    import config
+
+    print('Successfully imported "config"')
+
+    print('Bot has started...')
 except ImportError:
     print('Failed to import required modules, quitting...')
     exit(1)
 
-BOT_TOKEN = ''
+bot = telebot.TeleBot(token=config.token)
 
-bot = telebot.TeleBot(token=BOT_TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
